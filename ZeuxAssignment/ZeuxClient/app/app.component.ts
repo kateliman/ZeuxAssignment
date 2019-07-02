506a645bfa43886b35d8ddc903d3297100f5aaef
Kateryna Liman <kateryna.liman@itomy.ch>
2019-07-02 10:32:06 +0300

    fixed style

--- ZeuxAssignment/ZeuxClient/app/app.component.ts
+++ ZeuxAssignment/ZeuxClient/app/app.component.ts
@@ -1,29 +1,12 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-//import { ProductsService } from './products.service';
-//import { Product } from './product';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
-    //providers: [ProductsService]
 })
-export class AppComponent /*implements OnInit */{
+export class AppComponent {
     public outlet: RouterOutlet;
-    //get(outlet: RouterOutlet) {
-    //    return outlet && outlet.activatedRouteData;
-    //}
-    //public products: Product[];
-    //viewMode = 'All';
-
-    //constructor(private productsService: ProductsService) { }
-
-    //ngOnInit() {
-    //    this.getProducts();
-    //}
-    
-    //getProducts() {
-    //    this.productsService.getAllProducts()
-    //        .subscribe((data: Product[]) => this.products = data);
-    //}
+    public ActiveTab = "MyAssets";
+ 
 }
